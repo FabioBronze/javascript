831e452370a926203f535d68861297a61999da2b
FabioBronze <fabio.r.bronze@hotmail.com>
2023-01-04 18:53:37 +0000

    condições

---
+++ teste05/teste01.js
@@ -0,0 +1,9 @@
+let vel = 200
+
+console.log(`A sua velocidade é de ${vel} Km/h`)
+
+if (vel > 60) {
+    console.log(`Estás dentro do limite!`)
+} 
+
+console.log(`Dirija sempre com sinto de segurança!`)
---
+++ teste05/teste02.js
@@ -0,0 +1,9 @@
+var país = 'Suiça'
+
+console.log(`Vivendo em ${país}`)
+
+if(país == 'Portugal'){
+    console.log(`Português`)
+}else{
+    console.log(`Estrangeiro`)
+}
