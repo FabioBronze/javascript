59e089ede34fdc6db0f4a28c4d06487ece242bb9
FabioBronze <fabio.r.bronze@hotmail.com>
2023-01-05 15:06:10 +0000

    final

---
+++ exercicios/ex001/script.js
@@ -0,0 +1,19 @@
+function carregar(){
+    let msg = document.getElementById('msg')
+    let img = document.getElementById('imagem')
+    let data = new Date()
+    let hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        img.src = 'img/dia.jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'img/tarde.jpg'
+        document.body.style.background = '#b9846f'
+    } else {
+        img.src = 'img/noite.jpg'
+        document.body.style.background = '#515154'
+    }
+}
